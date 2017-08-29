f1e2766807f2e397cf542e92a07211614e97cf80
Michał Czyż <m.czyz@selleo.com>
2017-08-29 23:57:39 +0200

    setup linters

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,29 @@
 module.exports = {
+  globals: {
+    server: true,
+  },
   root: true,
   parserOptions: {
     ecmaVersion: 2017,
-    sourceType: 'module'
+    sourceType: 'module',
   },
-  extends: 'eslint:recommended',
+  extends: ['eslint:recommended', 'plugin:ember/recommended'],
   env: {
-    browser: true
+    browser: true,
   },
   rules: {
-  }
+    'no-console': [
+      1,
+      {
+        allow: ['warn', 'error'],
+      },
+    ],
+    'comma-dangle': [2, 'only-multiline'],
+    'space-before-function-paren': 'off',
+    'no-extra-semi': 2,
+    quotes: [2, 'single', 'avoid-escape'],
+    'ember/named-functions-in-promises': 'warn',
+    'ember/alias-model-in-controller': 'warn',
+    'ember/use-ember-get-and-set': 'warn',
+  },
 };
